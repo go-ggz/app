ce52fdb013472ce6d461cc575171b17a60319667
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-10-25 00:01:30 +0800

    refactor: Create the starter Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,14 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to ggz'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
